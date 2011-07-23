677815bf3b552c315a0cead12894cb0460b89f5e
SUZUKI Shinichiro <shinichiro.su@gmail.com>
2011-07-23 18:24:22 +0900

    BookmarkクラスとEntryクラスを追加

--- models.py
+++ models.py
@@ -5,11 +5,12 @@ from google.appengine.ext import db
 from google.appengine.ext import db
 from kay.auth.models import GoogleUser
 import kay.db
+from datetime import datetime
 
 # Create your models here.
 class MyUser(GoogleUser):
   def get_chara(self):
-    u"""ユーザーらキャラデータを取得する
+    u"""ユーザーキャラデータを取得する
     """
     charas = Chara.all().filter('user =', self).fetch(1)
     if charas:
@@ -27,18 +28,6 @@ class MyUser(GoogleUser):
 
     return chara
 
-# class Category(db.Model):
-#   name = db.StringProperty(required=True)
-
-#   def __unicode__(self):
-#     return self.name
-
-# class Comment(db.Model):
-#   user = kay.db.OwnerProperty()
-#   body = db.TextProperty(required=True, verbose_name=u'Your Comment!!')
-#   category = db.ReferenceProperty(Category, collection_name='comments')
-#   created = db.DateTimeProperty(auto_now_add=True)
-
 def value_or_email(value):
   u"""emailプロパティをもっていたらemailを返す。なければそのままの値を返す
   """
@@ -53,13 +42,6 @@ def value_or_email(value):
 class SsModel(db.Model):
   u"""モデルクラスの共通の親
   """
-
-#   def to_json(self):
-#     u"""オブジェクトをJSONにして返す
-#     """
-#     attr_list = [(attr, _value_or_key(getattr(self, attr))) for attr in self._all_properties]
-#     attr_dict = dict(attr_list)
-#     return json.dumps(attr_dict)
 
 class Chara(SsModel):
   u"""ユーザーキャラクターデータ
@@ -78,3 +60,104 @@ class Chara(SsModel):
     attr_list = [(attr, value_or_email(getattr(self, attr))) for attr in self._all_properties]
     attr_dict = dict(attr_list)
     return json.dumps(attr_dict)
+
+class Entry(SsModel):
+  u"""はてぶエントリーページ（ダンジョンと見なす）
+  """
+  title      = db.TextProperty()    # タイトル
+  count      = db.IntegerProperty() # ブックマークしている合計ユーザ数
+  url        = db.StringProperty(required=True) # ブックマークされているURL
+  entry_url  = db.StringProperty(required=True) # はてなブックマークエントリーページのURL
+  screenshot = db.StringProperty()      # スクリーンショット画像のURL
+  eid        = db.IntegerProperty(required=True)     # エントリーID
+
+  @classmethod
+  def get_entry(cls, url):
+    u"""urlからエントリデータを取得する
+    """
+    entries = cls.all().filter('url =', url).fetch(1)
+    if entries:
+      entry = entries[0]
+    else:
+      htb = cls.get_hatebu_api(url)
+      entry = cls.add_entry(htb)
+
+    return entry
+
+#   @classmethod
+#   def get_hatebu_api(cls, url):
+#     api_url = "http://b.hatena.ne.jp/entry/jsonlite/"
+#     htb_json = urllib.urlopen(api_url + url).read()
+#     htb = json.loads(htb_json)
+
+#     return htb
+
+  @classmethod
+  def add_entry(cls,
+                url, entry_url, eid,
+                title = '', count = 0, 
+                screenshot = '', bookmarks = {}):
+    u"""エントリデータを登録する
+    """
+
+    entry = cls(
+      title      = title,
+      count      = count,
+      url        = url,
+      entry_url  = entry_url,
+      screenshot = screenshot,
+      eid        = eid,
+    )
+    entry.put()
+    Bookmark.add_bookmarks(bookmarks, entry);
+
+    return entry
+
+#   def explore(self, user):
+#     u"""
+#     """
+#     bookmarks = self.get_bookmarks()
+
+#     return bookmarks
+
+#   def get_bookmarks(self):
+#     u"""
+#     """
+#     return Bookmark.get_bookmarks(self)
+
+
+class Bookmark(SsModel):
+  u"""はてぶエントリーページについてるブックマーク（モンスターとみなす）
+  """
+  user      = db.StringProperty(required=True)       # ブックマークしたユーザ名
+  tags      = db.StringListProperty()   # タグの配列
+  timestamp = db.DateTimeProperty() # ブックマークした時刻。new Date(timestamp) で JavaScript の Date オブジェクトになります
+  comment   = db.StringProperty()   # ブックマークコメント
+  entry     = db.ReferenceProperty(Entry, required=True) # 所属エントリ
+
+  @classmethod
+  def add_bookmarks(cls, bookmarks, entry):
+    u"""ブックマークデータを登録する
+    """
+    for bm in bookmarks:
+      if not bm.has_key('user'): continue
+
+      timestamp = datetime.strptime(bm['timestamp'], "%Y/%m/%d %H:%M:%S")
+      bookmark = cls(
+        user      = bm.get('user'),
+        tags      = bm.get('tags'),
+        timestamp = timestamp,
+        comment   = bm.get('comment'),
+        entry     = entry,
+      )
+      bookmark.put()
+
+    return
+
+#   @classmethod
+#   def get_bookmarks(cls, entry):
+#     u"""指定したエントリにひもづくブックマークデータを取得する
+#     """
+#     bookmarks = cls.all().filter('entry =', entry).fetch(100)
+
+#     return json.dumps(bookmarks)
--- tests/__init__.py
+++ tests/__init__.py
@@ -2,14 +2,16 @@
 from django.utils import simplejson as json
 from google.appengine.ext import db
 from kay.ext.testutils.gae_test_base import GAETestBase
+from datetime import datetime
 from htbrpg2kay.models import (
-  MyUser, Chara,
+  MyUser, Chara, Entry, Bookmark,
 )
+
 
 class ModelTest(GAETestBase):
   def tearDown(self):
-    db.delete(MyUser.all().fetch(10))
-    db.delete(Chara.all().fetch(10))
+    for c in [MyUser, Chara, Entry, Bookmark]:
+      db.delete(c.all().fetch(100))
 
   def test_myuser(self):
     user = MyUser(email='test@example.com', first_name=u'testuser')
@@ -44,3 +46,81 @@ class ModelTest(GAETestBase):
                         'magic':1,
                         'speed':1,
                         'user': u.email}))
+
+  def test_entry(self):
+    e = Entry(title = u'タイトル',
+              count = 10,
+              url = 'http://hoge.com',
+              entry_url = 'http://b.hatena.ne.jp/hoge',
+              eid = 1)
+    e.put()
+
+    # get_entry
+    e2 = Entry(title = u'タイトル2',
+              count = 7,
+              url = 'http://fuga.com',
+              entry_url = 'http://b.hatena.ne.jp/fuga',
+              eid = 2)
+    e2.put()
+    es = Entry.all().fetch(100)
+    self.assertEquals(len(es), 2)
+    e_ = Entry.get_entry('http://fuga.com')
+    self.assertEquals(e_.title, u'タイトル2')
+    self.assertEquals(e_.count, 7)
+    self.assertEquals(e_.url, 'http://fuga.com')
+    self.assertEquals(e_.entry_url, 'http://b.hatena.ne.jp/fuga')
+    self.assertEquals(e_.eid, 2)
+
+    # add_entry
+    e3 = Entry.add_entry(title = u'htbタイトル',
+                         count= 3,
+                         url= 'http://example.jp',
+                         entry_url='http://entry_url.com',
+                         eid= 3)
+    es_ = Entry.all().fetch(100)
+    self.assertEquals(len(es_), 3)
+    self.assertEquals(es_[2].url, 'http://example.jp')
+#     self.assertEquals(len(es_), 3)
+#     self.assertEquals(es_[2].url, 'http://example.jp')
+
+  def test_bookmark(self):
+    u = MyUser(email = 'suzuki@hoge.com', first_name = 'shin')
+    u.put()
+    e = Entry(title = u'タイトル4',
+              count = 7,
+              url = 'http://fuga.com',
+              entry_url = 'http://b.hatena.ne.jp/fuga',
+              eid = 4)
+    e.put()
+    b = Bookmark(user = u'jojo',
+                 tags = ["aaa", "bbb"],
+                 timestamp = datetime.strptime("2011/07/22 21:22:06",  "%Y/%m/%d %H:%M:%S"),
+                 comment = u'コメント',
+                 entry = e)
+    b.put()
+    bs = Bookmark.all().fetch(100)
+    self.assertEquals(len(bs), 1)
+    # add_bookmarks
+    e2 = Entry(title = u'タイトル5',
+              count = 100,
+              url = 'http://aa.fuga.com',
+              entry_url = 'http://b.hatena.ne.jp/fugaxxx',
+              eid = 5)
+    e2.put()
+    bookmarks = [
+      {u'comment': u'',
+       u'tags': [u'api'],
+       u'timestamp': u'2011/07/22 15:36:47',
+       u'user': u'cubick'},
+      {u'comment': u'\u3053\u308c\u4f7f\u3063\u3066\u306a\u3093\u304b\u3084\u308a\u305f\u3044',
+       u'tags': [u'hatena'],
+       u'timestamp': u'2011/07/05 23:51:19',
+       u'user': u'takuma510'},
+    ]
+    Bookmark.add_bookmarks(bookmarks, e2)
+    bs2 = Bookmark.all().fetch(100)
+    self.assertEquals(len(bs2), 3)
+    self.assertEquals(bs2[1].user, u'cubick')
+    # get_bookmarks
+#     bs3 = Bookmark.get_bookmarks(e)
+#     dir(bs3)
